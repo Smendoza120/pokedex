47c9030500fa62e9789098ba94669d570b530ccf
SMendoza120 <98554237+Smendoza120@users.noreply.github.com>
2025-06-01 13:05:10 -0500

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import WelcomeView from "@/views/WelcomeView.vue";
 import HomeView from "@/views/HomeView.vue";
 import LoadingSpinner from "@/components/LoadingSpinner.vue";
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: "/",
